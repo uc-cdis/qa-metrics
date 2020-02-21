99b93583ed0c87eaf178c6c05c3bb02422d4c63c
Marcelo Costa <marceloc@uchicago.edu>
2020-02-21 17:42:04 -0600

    update with release 2020.03 info

--- jobs/current_release.rb
+++ jobs/current_release.rb
@@ -1,5 +1,5 @@
 releases = Hash.new({ value: 0 })
 SCHEDULER.every '1m', :first_in => 0 do |job|
-  releases['current'] = { label: '->', value: 'integration202002' }
+  releases['current'] = { label: '->', value: '2020.03' }
   send_event('current_release', { items: releases.values } )
 end
